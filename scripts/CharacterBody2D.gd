21b4db5660b287a0ada73b673771133bf2a5799f
Tomas Votava <thomasvotava@seznam.cz>
2024-04-05 21:06:15 +0200

    Changed the whole thing
    LULW

--- scripts/CharacterBody2D.gd
+++ scripts/CharacterBody2D.gd
@@ -10,7 +10,7 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
-		velocity.y += gravity * delta
+		velocity.y += gravity * delta 
 
 	# Handle jump.
 	if Input.is_action_just_pressed("Jump") and is_on_floor():
