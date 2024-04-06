083864e97e459f8c1558c7183d1535fa6ca584a2
Daniel Griger <buficek.craft@gmail.com>
2024-04-06 16:22:05 +0200

    skibidi

---
+++ scripts/Order_Timer.gd
@@ -0,0 +1,18 @@
+extends Control
+
+@onready var Progress : ProgressBar = $ProgressBar
+@onready var _Timer : Timer = $Timer
+signal  TimeOut
+
+func _ready():
+	pass
+
+func _process(delta):
+	Progress.value = _Timer.process_callback
+
+func start_timer(time):
+	Progress.max_value = time
+	_Timer.start(time)
+
+func _on_timer_timeout():
+	TimeOut.emit()
