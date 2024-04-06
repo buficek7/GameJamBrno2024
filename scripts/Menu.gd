c5219101993778b45ec875b6e24ddc93814f680b
Daniel Griger <buficek.craft@gmail.com>
2024-04-06 19:00:57 +0200

    Menu

---
+++ scripts/Menu.gd
@@ -0,0 +1,24 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_start_pressed():
+	get_tree().change_scene_to_file("res://scenes/Test Scene.tscn")
+
+
+func _on_quit_pressed():
+	get_tree().quit()
+
+
+func _on_autori_pressed():
+	get_tree().change_scene_to_file("res://scenes/Credits.tscn")
+
