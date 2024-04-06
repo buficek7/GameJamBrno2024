b3812f8fa72916cd486b3986cf8d2bd7e34fe865
Tomas Votava <thomasvotava@seznam.cz>
2024-04-06 12:55:53 +0200

    Added mouse_hover to dish

--- scripts/Dish.gd
+++ scripts/Dish.gd
@@ -6,6 +6,7 @@ var ingredients = {} #dictionary of put ingredients
 @onready var parent = get_node("../../Node2D")
 @export var scaleTexture = Vector2(0.1, 0.1)
 @export var scaleMax = 0.05
+var mouse_hover = false
 var score = 0
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -24,17 +25,24 @@ func ingEmpty():
 	return len(ingredients.keys()) == 0
 
 func _input(event):
+	if not mouse_hover:
+		return
 	if event is InputEventMouseButton:
 		if ingEmpty():
 			return
 		dish_ready()
 		return
 
+func _mouse_enter():
+	mouse_hover = true
+
+func _mouse_exit():
+	mouse_hover = false
+
 func addIngredient(ingredient):
 	ingredients[ingredient] = ingredients.get(ingredient, 0) + 1
 
 func dish_ready():
-	return
 	var recipe: Order = parent.get_order() #code to get value of recipe
 	var recipe_ing: Dictionary = Recipes.get_recipe(recipe.get_name())
 	#should check and implement losing function
