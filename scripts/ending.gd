cb98e66ef13df9523c918bdb2c0dd59b6995d105
Tomas Votava <thomasvotava@seznam.cz>
2024-04-07 15:03:19 +0200

    Yeah

--- scripts/ending.gd
+++ scripts/ending.gd
@@ -9,12 +9,14 @@ Final Score: %d
 [/font_size]
 [/center]
 "
+@onready var woman : AudioStreamPlayer = $"../woman"
 
 func show_screen():
 	visible = true
 	await get_tree().create_timer(0.5).timeout
-	#play sound
-	await get_tree().create_timer(1).timeout
+	woman.play()
+	await get_tree().create_timer(2).timeout
+	woman.stop()
 	text_score.text = new_text % finalscore
 	text_score.visible = true
 	await get_tree().create_timer(1).timeout
